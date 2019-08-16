4a9f489ee5600d01bba50d1a060e4b1c11b06bdd
Jenn Soderborg <jsoderborg7@gmail.com>
2019-08-16 11:20:06 -0600

    stuff is showing on the page, trying to style it so only one person shows at a time

--- starwars/src/components/Person.js
+++ starwars/src/components/Person.js
@@ -3,28 +3,19 @@ import React from 'react';
 const Person = props => {
   return (
     <div>
-      <h1>{props.name}</h1>
+      <div>
+        <h1>{props.name}</h1>
+      </div>
+      <div>
+        <h2>Identify me:</h2>
+        <p>Hair:{props.hair_color}</p>
+        <p>Eyes:{props.eye_color}</p>
+        <p>Skin:{props.skin_color}</p>
+        <p>Height:{props.height} inches</p>
+        <p>Weight:{props.mass} kg</p>
+      </div>
     </div>
-    <div>
-      <h2>What you should know about me:</h2>
-      <p>{props.birth_year}</p>
-      <p>{props.homeworld}</p>
-      <p>{props.species}</p>
-    </div>
-    <div>
-      <h2>What I look like:</h2>
-      <p>{props.hair_color}</p>
-      <p>{props.eye_color}</p>
-      <p>{props.skin_color}</p>
-      <p>{props.height}</p>
-      <p>{props.mass}</p>
-    </div>
-    <div>
-      <h2>Cool stuff I have:</h2>
-      <p>{props.starships}</p>
-      <p>{props.vehicles}</p>
-    </div>
-  );
-};
+  )
+}
 
 export default Person;
