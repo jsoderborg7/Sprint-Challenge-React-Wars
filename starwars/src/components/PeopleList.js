ba9b7dcf7d6dbeedead81056de0bb19670715f3b
Jenn Soderborg <jsoderborg7@gmail.com>
2019-08-16 10:21:19 -0600

    components mostly finished, need to add map functions, nothing displaying yet

--- starwars/src/components/PeopleList.js
+++ starwars/src/components/PeopleList.js
@@ -1,8 +1,14 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
+import Name from "./Name";
+import Appearance from "./Appearance";
+import VitalStats from "./VitalStats";
+import Films from "./Films";
+import Starships from "./Starships";
+import Vehicles from "./Vehicles";
+import Media from "./Media";
 
-
-const PeopleList = ()=>{
+const PeopleList = () =>{
 
 const [name, setName] = useState();
 const [birth_year, setBirth_year] = useState();
@@ -21,28 +27,36 @@ const [media, setMedia] = useState();
 
 useEffect(() =>{
   axios
-    .get(`"https://swapi.co/api/people/"`)
+    .get(`https://swapi.co/api/people/`)
     .then(response =>{
-      setName(response.data.name)
-      setBirth_year(response.data.birth_year)
-      setEye_color(response.data.eye_color)
-      setGender(response.data.gender)
-      setHair_color(response.data.hair_color)
-      setHeight(response.data.height)
-      setMass(response.data.mass)
-      setSkin_color(response.data.skin_color)
-      setHomeworld(response.data.homeworld)
-      setFilms(response.data.films)
-      setSpecies(response.data.species)
-      setStarships(response.data.starships)
-      setVehicles(response.data.vehicles)
-      setMedia(response.data.media)
-    }),
+      setName(response.data.name);
+      setBirth_year(response.data.birth_year);
+      setEye_color(response.data.eye_color);
+      setGender(response.data.gender);
+      setHair_color(response.data.hair_color);
+      setHeight(response.data.height);
+      setMass(response.data.mass);
+      setSkin_color(response.data.skin_color);
+      setHomeworld(response.data.homeworld);
+      setFilms(response.data.films);
+      setSpecies(response.data.species);
+      setStarships(response.data.starships);
+      setVehicles(response.data.vehicles);
+      setMedia(response.data.media);
+    });
 },[]);
 
 return (
   <div>
-    
+    <>
+      <Name name={name} />
+      <Media media={media} />
+      <Appearance eye_color={eye_color} hair_color={hair_color} skin_color={skin_color} height={height} mass={mass} />
+      <VitalStats birth_year={birth_year} gender={gender} homeworld={homeworld} species={species} />
+      <Starships starships={starships} />
+      <Vehicles vehicles={vehicles} />
+      <Films films={films} />
+    </>
   </div>
 )
 }
